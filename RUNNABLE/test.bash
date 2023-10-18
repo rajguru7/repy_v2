c972c76c825d7144a0d9ba73a74f60120d1e822e
Saurabh Rajguru <saurabhrajguru@xps.local>
2023-10-18 12:22:12 -0400

    pyenv created, build added for Ubuntu, ass2.1 done

---
+++ RUNNABLE/test.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+RED='\033[0;31m'
+CYAN='\033[1;36m'
+NC='\033[0m'
+
+referencemonitor="reference_monitor_sr6895.r2py"
+
+#for testcase in old_sr6895_*; do 
+#    if grep -q "undo()" "$testcase"; then
+#        echo -e "\n${RED}Testing repy: ${CYAN}$testcase${NC}"
+#        echo -e "Not applicable"
+#    else
+#        echo -e "\n${RED}Testing repy: ${CYAN}$testcase${NC}"
+#        python repy.py restrictions.default encasementlib.r2py $testcase
+#    fi
+#    printf '\n%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+#done
+
+for testcase in sr6895_*; do 
+    echo -e "\n${RED}Testing my code: ${CYAN}$testcase${NC}"
+    python repy.py restrictions.default encasementlib.r2py $referencemonitor $testcase
+    printf '\n%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+done
